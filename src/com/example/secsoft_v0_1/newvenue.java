3ece95b0649d7a6c37c7c544800d329ae47ed3ec
clubsec <clubsecsecurity@gmail.com>
2013-03-06 16:33:59 +0000

    First upload.

---
+++ src/com/example/secsoft_v0_1/newvenue.java
@@ -0,0 +1,16 @@
+package com.example.secsoft_v0_1;
+
+import android.app.Activity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cybrix
+ * Date: 06/03/13
+ * Time: 16:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class newvenue extends Activity
+{
+
+
+}
